6cf719ad242a3ef491ff2a1a5e37baf19d879ac6
Daniela Noir <daniela@danielanoir.com>
2018-02-12 07:14:21 -0500

    added toggle between all tabs

--- scripts.js
+++ scripts.js
@@ -2,13 +2,61 @@ $(document).ready(function(){
 //show Rooms and Rates tab
   $(".roomsLink").click(function(){
     $(".aboutContainer").hide();
-    $(".roomsAndRates").show();
+    $(".roomsAndRatesContainer").show();
+    $(".theAreaContainer").hide();
+    $(".addOnsContainer").hide();
+    $(".directionsContainer").hide();
+    $(".contactContainer").hide();
   });
 
 //show About tab
   $(".aboutLink").click(function(){
     $(".aboutContainer").show();
-    $(".roomsAndRates").hide();
+    $(".roomsAndRatesContainer").hide();
+    $(".theAreaContainer").hide();
+    $(".addOnsContainer").hide();
+    $(".directionsContainer").hide();
+    $(".contactContainer").hide();
+  });
+
+//show The Area tab
+  $(".areaLink").click(function(){
+    $(".aboutContainer").hide();
+    $(".roomsAndRatesContainer").hide();
+    $(".theAreaContainer").show();
+    $(".addOnsContainer").hide();
+    $(".directionsContainer").hide();
+    $(".contactContainer").hide();
+  });
+
+//show Add Ons tab
+  $(".addOnsLink").click(function(){
+    $(".aboutContainer").hide();
+    $(".roomsAndRatesContainer").hide();
+    $(".theAreaContainer").hide();
+    $(".addOnsContainer").show();
+    $(".directionsContainer").hide();
+    $(".contactContainer").hide();
+  });
+
+//show Directions tab
+  $(".directionsLink").click(function(){
+    $(".aboutContainer").hide();
+    $(".roomsAndRatesContainer").hide();
+    $(".theAreaContainer").hide();
+    $(".addOnsContainer").hide();
+    $(".directionsContainer").show();
+    $(".contactContainer").hide();
+  });
+
+//show Contact tab
+  $(".contactLink").click(function(){
+    $(".aboutContainer").hide();
+    $(".roomsAndRatesContainer").hide();
+    $(".theAreaContainer").hide();
+    $(".addOnsContainer").hide();
+    $(".directionsContainer").hide();
+    $(".contactContainer").show();
   });
 
 
